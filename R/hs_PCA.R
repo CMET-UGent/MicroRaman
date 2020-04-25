5e68ebf24b193979fa120ebbf43f9d2b3c16dfa1
rprops <Ruben.Props@UGent.be>
2020-04-25 10:17:05 +0200

    add tsne/PCA for hs objects

---
+++ R/hs_PCA.R
@@ -0,0 +1,29 @@
+#' Run PCA on a hyperspec object
+#'
+#' @param hs.x hyperSpec object
+#' @param scale Whether scaling should be applied to the data (centering + scaling)
+#' @importFrom stats prcomp
+#' @examples
+#' ## Short example
+#'
+#' # Load hyperSpec object
+#' data("hs.example")
+#'
+#' # Convert to MassSpectrum object
+#' hs.x.proc <- hs_preprocess(hs.x)
+#'
+#' df.PCA <- hs_PCA(hs.x.proc)
+#' @export
+
+
+hs_PCA <- function(hs.x, ...) {
+  if (is.null(rownames(hs.x@data$spc)))
+    spectra_ids <-
+      hs.x@data$filename
+  else
+    spectra_ids <- rownames(hs.x@data$spc)
+  hs.PCA <- prcomp(x = hs.x@data$spc, ...)
+  hs.PCA.results <- data.frame(Spectrum_ID = spectra_ids,
+    hs.PCA$x)
+  return(hs.PCA.results)
+}
---
+++ R/hs_tsne.R
@@ -0,0 +1,29 @@
+#' Run tSNE on a hyperspec object
+#'
+#' @param hs.x hyperSpec object or alternatively distance matrix
+#' @importFrom tsne tsne
+#' @examples
+#' ## Short example
+#'
+#' # Load hyperSpec object
+#' data("hs.example")
+#'
+#' # Convert to MassSpectrum object
+#' hs.x.proc <- hs_preprocess(hs.x)
+#'
+#' df.tsne <- hs_tsne(hs.x.proc)
+#' @export
+
+hs_tsne <- function(hs.x, ...){
+  # Extract spectra ID
+  if (is.null(rownames(hs.x@data$spc)))
+    spectra_ids <-
+      hs.x@data$filename
+  else
+    spectra_ids <- rownames(hs.x@data$spc)
+  # Run tSNE
+  hs.tsne <- tsne(X = hs.x@data$spc, ...)
+  hs.tsne.results <- data.frame(Spectrum_ID = spectra_ids,
+    hs.tsne)
+  return(hs.tsne.results)
+}

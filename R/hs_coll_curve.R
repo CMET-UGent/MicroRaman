6f408a56dff58caab4d9668e43d51255b7c08168
rprops <Ruben.Props@UGent.be>
2020-04-28 11:02:34 +0200

    add imports for plot

--- R/hs_coll_curve.R
+++ R/hs_coll_curve.R
@@ -14,6 +14,8 @@
 #' @param plot_fig Should figures of collectors curves be shown? Defaults to TRUE.
 #' @importFrom dplyr group_by summarize
 #' @importFrom magrittr %>%
+#' @importFrom ggplot2 geom_point theme_bw geom_smooth geom_errorbar labs theme ylim ggplot
+#' @importFrom cowplot plot_grid
 #' @examples
 #' # Short example
 #' data("hs_example")
@@ -22,7 +24,7 @@
 #' hs_example <- hs_preprocess(hs_example)
 #'
 #' # Calculate metrics
-#' hs_coll_curve(hs_example, intervals = 3, nboot = 10)
+#' hs_coll_curve(hs_example, intervals = 3, nboot = 10, plot_fig = FALSE)
 #' @export
 #'
 hs_coll_curve <- function(hs.x,
@@ -62,37 +64,37 @@ hs_coll_curve <- function(hs.x,
 
   # Plot the results
   if(plot_fig){
-    p1 <- ggplot2::ggplot(results_coll, aes(x = nspec, y = D0.mean))+
-      ggplot2::geom_point(size = 3, shape = 21)+
-      ggplot2::theme_bw()+
-      ggplot2::geom_smooth(se = FALSE, col = "black")+
-      ggplot2::geom_errorbar(aes(ymin = D0.mean - D0.sd, ymax = D0.mean + D0.sd), width = 0.02)+
-      ggplot2::labs(y = "D0", x = "number of spectra")+
-      ggplot2::theme(axis.text = element_text(size = 12),
+    p1 <- ggplot(results_coll, aes(x = nspec, y = D0.mean))+
+      geom_point(size = 3, shape = 21)+
+      theme_bw()+
+      geom_smooth(se = FALSE, col = "black")+
+      geom_errorbar(aes(ymin = D0.mean - D0.sd, ymax = D0.mean + D0.sd), width = 0.02)+
+      labs(y = "D0", x = "number of spectra")+
+      theme(axis.text = element_text(size = 12),
         axis.title = element_text(size = 12))+
-      ggplot2::ylim(0, 1.1*max(results_coll$D0.mean))
+      ylim(0, 1.1*max(results_coll$D0.mean))
 
-    p2 <- ggplot2::ggplot(results_coll, aes(x = nspec, y = D1.mean))+
-      ggplot2::geom_point(size = 3, shape = 21)+
-      ggplot2::theme_bw()+
-      ggplot2::geom_smooth(se = FALSE, col = "black")+
-      ggplot2::geom_errorbar(aes(ymin = D1.mean - D1.sd, ymax = D1.mean + D1.sd), width = 0.02)+
-      ggplot2::labs(y = "D1", x = "number of spectra")+
-      ggplot2::theme(axis.text = element_text(size = 12),
+    p2 <- ggplot(results_coll, aes(x = nspec, y = D1.mean))+
+      geom_point(size = 3, shape = 21)+
+      theme_bw()+
+      geom_smooth(se = FALSE, col = "black")+
+      geom_errorbar(aes(ymin = D1.mean - D1.sd, ymax = D1.mean + D1.sd), width = 0.02)+
+      labs(y = "D1", x = "number of spectra")+
+      theme(axis.text = element_text(size = 12),
         axis.title = element_text(size = 12))+
-      ggplot2::ylim(0, 1.1*max(results_coll$D0.mean))
+      ylim(0, 1.1*max(results_coll$D0.mean))
 
-    p3 <- ggplot2::ggplot(results_coll, aes(x = nspec, y = D2.mean))+
-      ggplot2::geom_point(size = 3, shape = 21)+
-      ggplot2::theme_bw()+
-      ggplot2::geom_smooth(se = FALSE, col = "black")+
-      ggplot2::geom_errorbar(aes(ymin = D2.mean - D2.sd, ymax = D2.mean + D2.sd), width = 0.02)+
-      ggplot2::labs(y = "D2", x = "number of spectra")+
-      ggplot2::theme(axis.text = element_text(size = 12),
+    p3 <- ggplot(results_coll, aes(x = nspec, y = D2.mean))+
+      geom_point(size = 3, shape = 21)+
+      theme_bw()+
+      geom_smooth(se = FALSE, col = "black")+
+      geom_errorbar(aes(ymin = D2.mean - D2.sd, ymax = D2.mean + D2.sd), width = 0.02)+
+      labs(y = "D2", x = "number of spectra")+
+      theme(axis.text = element_text(size = 12),
         axis.title = element_text(size = 12))+
-      ggplot2::ylim(0, 1.1*max(results_coll$D0.mean))
+      ylim(0, 1.1*max(results_coll$D0.mean))
 
-    print(cowplot::plot_grid(p1, p2, p3, nrow = 1))
+    print(plot_grid(p1, p2, p3, nrow = 1))
   }
 
   return(results_coll)
